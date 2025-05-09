8d4cb4c95fba333f41d7705af7480c1b5900a814
HeedzZ <yann.mostowski@gmail.com>
2025-05-10 01:18:58 +0200

    fix: Fix vercel anali

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Analytics } from '@vercel/analytics/react';
 import App from './App.tsx';
 import './index.css';
+
+// Analytics component
+const AnalyticsWrapper = () => {
+  // On n'utilise Analytics que dans l'environnement de production
+  if (import.meta.env.PROD) {
+    // Cette ligne sera omise en développement
+    const { Analytics } = require('@vercel/analytics/react');
+    return <Analytics />;
+  }
+  return null;
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Analytics />
+    <AnalyticsWrapper />
   </StrictMode>
 );
